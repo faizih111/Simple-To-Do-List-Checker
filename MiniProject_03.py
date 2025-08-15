219aa32e4217af585e0d6a688336e3051c9696a5
faizih111 <eng.faizan11@gmail.com>
2025-08-15 11:10:26 +0500

    Create MiniProject_03.py

---
+++ MiniProject_03.py
@@ -0,0 +1,12 @@
+print("--- To-Do List ---")
+task=['Homework', 'Laundry', 'Grocery Shopping', 'Exercise', 'Read Book']
+task.append('Call Friend')
+print(task)
+if 'Homework' in task:
+    print("Total tasks:",len(task))
+print("--- Shopping List ---")
+item=['Apples', 'Bananas', 'Bread', 'Milk', 'Eggs']
+item.append('Juice')
+print(item)
+if 'Juice' in item:
+    print("Total tasks:",len(item))
