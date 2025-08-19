1036ee2a58d5ad56998be37eaed8fc54a48172cd
faizih111 <eng.faizan11@gmail.com>
2025-08-19 13:59:13 +0500

    update code

--- MiniProject_03.py
+++ MiniProject_03.py
@@ -1,12 +1,16 @@
 print("--- To-Do List ---")
-task=['Homework', 'Laundry', 'Grocery Shopping', 'Exercise', 'Read Book']
-task.append('Call Friend')
+
+task = ["Homework", "Laundry", "Grocery Shopping", "Exercise", "Read Book"]
+task.append("Call Friend")
 print(task)
-if 'Homework' in task:
-    print("Total tasks:",len(task))
+
+if "Homework" in task:
+    print("Total tasks:", len(task))
+
 print("--- Shopping List ---")
-item=['Apples', 'Bananas', 'Bread', 'Milk', 'Eggs']
-item.append('Juice')
+item = ["Apples", "Bananas", "Bread", "Milk", "Eggs"]
+item.append("Juice")
 print(item)
-if 'Juice' in item:
-    print("Total tasks:",len(item))
+
+if "Juice" in item:
+    print("Total tasks:", len(item))
